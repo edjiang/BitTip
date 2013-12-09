8c6a7c50328cc2cb4e919bcf7a0e747d76296cb9
Edward Jiang <edjiang@uw.edu>
2013-12-08 21:54:02 -0800

    fixing redirect

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 if($_SERVER['HTTP_HOST'] == 'redeem.bittip.co')
 	header("Location: http://www.bittip.co/redeem.htm",TRUE,301);
-if($_SERVER['REQUEST_URI'] == '/')
+else if($_SERVER['REQUEST_URI'] == '/')
 	header("Location: http://www.bittip.co/index.htm",TRUE,301);
+?>
